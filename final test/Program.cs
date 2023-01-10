45f302adf911079541a4e1e747349a2ad6b0c476
сергей <dman.2055@yandex.ru>
2023-01-10 20:43:22 +0300

    2

--- final test/Program.cs
+++ final test/Program.cs
@@ -7,3 +7,15 @@ arr[3] = "Have a good day";
 arr[4] = "!";
 
 
+int FindSizeOfArr(string [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
